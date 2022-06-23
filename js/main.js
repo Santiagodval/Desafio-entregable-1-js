8ca6498707dcdd77895339367b1e2d5cf3b0dda9
Santiago Valdez <santiagodval@outlook.com>
2022-06-23 00:52:14 -0300

    empezado el ejercicio de arrays y objetos y solucionado problema con la numeracion de incisos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-inputType = parseInt(prompt("Ingrese el tipo de entrada que desea añadir al cuestionario(escribir el número correspondiente): \n 1-Multiple choice sin repetición \n 2-Multiple choice con repetición \n 3-Textfield"));
-let HTML = "";
 
+let HTML = "";
+const listaObjetos = [];
+
+//questionario se escribe igual en español que en inglés
+class cuestionarioSubsection{
+
+    constructor(type,title,questions){
+        this.type = type;
+        this.title = title;
+        this.questions = questions;
+    }
+
+}
+
+let cuestionarioSubsections = [];
+
+inputType = parseInt(prompt("Ingrese el tipo de entrada que desea añadir al cuestionario(escribir el número correspondiente): \n 1-Multiple choice sin repetición \n 2-Multiple choice con repetición \n 3-Textfield"));
 while(inputType!=null){
     switch (inputType) {
     case 1:
@@ -21,16 +36,20 @@ inputType = parseInt(prompt("Ingrese el tipo de entrada que desea añadir al cue
 
 
 function MC1() {
-    HTML = HTML + "<h3>" + prompt("Ingrese el título de la sección del cuestionario") + "</h3> \n"
+    let titulo =  prompt("Ingrese el título de la sección del cuestionario");
+    HTML = HTML + "<h3>" + titulo + "</h3> \n"
     let valor = "";
     let i = 1;
+    let valores=[];
     valor = prompt("Ingrese el texto de la opción" + i);
     while (valor != null) {
-
+        i++
+        valores.push(valor);
         HTML = HTML + "<input type='radio' value='" + valor + "'>" + valor + " <br> \n"
         valor = prompt("Ingrese el texto de la opción " + i);
     }
-
+    cuestionarioSubsections.push(new cuestionarioSubsection("MC1",titulo,valores));
+    console.log(cuestionarioSubsections);
     alert(HTML);
 }
 
@@ -40,7 +59,7 @@ function MC2() {
     let i = 1;
     valor = prompt("Ingrese el texto de la primera opción");
     while (valor != null) {
-
+        i++
         HTML = HTML + "<input type='checkbox' value='" + valor + "'>" + valor + " <br> \n"
         valor = prompt("Ingrese el texto de la opción " + i);
     }
