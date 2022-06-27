9cae75a945745aea3d06b4db2b0433600317f087
Santiago Valdez <santiagodval@outlook.com>
2022-06-27 19:20:00 -0300

    supuestamente terminado pero no ejecuta

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 let HTML = "";
-const listaObjetos = [];
 
 //questionario se escribe igual en español que en inglés
 class cuestionarioSubsection {
@@ -11,11 +10,16 @@ class cuestionarioSubsection {
         this.questions = questions;
     }
 
+    constructor(type,title){
+        this.type = type;
+        this.title = title;
+    }
+
 }
 
 let cuestionarioSubsections = [];
 
-inputType = parseInt(prompt("Ingrese el tipo de entrada que desea añadir al cuestionario(escribir el número correspondiente): \n 1-Multiple choice sin repetición \n 2-Multiple choice con repetición \n 3-Textfield"));
+inputType = parseInt(prompt("Ingrese el tipo de entrada que desea añadir al cuestionario(escribir el número correspondiente): \n 1-Multiple choice sin repetición \n 2-Multiple choice con repetición \n 3-Textfield \n cancelar-salir del bucle"));
 while (inputType != NaN) {
     switch (inputType) {
         case 1:
@@ -27,7 +31,6 @@ while (inputType != NaN) {
         case 3:
             Textfield();
             break;
-
         default:
             break;
     }
@@ -37,43 +40,53 @@ while (inputType != NaN) {
 
 
 function MC1() {
-    let titulo = prompt("Ingrese el título de la sección del cuestionario");
-    HTML = HTML + "<h3>" + titulo + "</h3> \n"
-    let valor = "";
+    let title = prompt("Ingrese el título de la sección del cuestionario");
+    HTML = HTML + "<h3>" + title + "</h3> \n"
+    let value = "";
     let i = 1;
-    let valores = [];
-    valor = prompt("Ingrese el texto de la opción" + i);
-    while (valor != null) {
+    let values = [];
+    value = prompt("Ingrese el texto de la opción" + i);
+    while (value != null) {
         i++
-        valores.push(valor);
-        HTML = HTML + "<input type='radio' value='" + valor + "'>" + valor + " <br> \n"
-        valor = prompt("Ingrese el texto de la opción " + i);
+        values.push(value);
+        HTML = HTML + "<input type='radio' value='" + value + "'>" + value + " <br> \n"
+        value = prompt("Ingrese el texto de la opción " + i);
     }
-    cuestionarioSubsections.push(new cuestionarioSubsection("MC1", titulo, valores));
+    cuestionarioSubsections.push(new cuestionarioSubsection("MC1", title, values));
     console.log(cuestionarioSubsections);
     alert(HTML);
 }
 
 function MC2() {
-    HTML = HTML + "<h3>" + prompt("Ingrese el título de la sección del cuestionario") + "</h3> \n"
-    let valor = "";
+    let title = prompt("Ingrese el título de la sección del cuestionario");
+    HTML = HTML + "<h3>" + title + "</h3> \n"
+    let value = "";
     let i = 1;
-    valor = prompt("Ingrese el texto de la primera opción");
-    while (valor != null) {
+    let values = [];
+    value = prompt("Ingrese el texto de la primera opción");
+    while (value != null) {
         i++
-        HTML = HTML + "<input type='checkbox' value='" + valor + "'>" + valor + " <br> \n"
-        valor = prompt("Ingrese el texto de la opción " + i);
+        values.push(value)
+        HTML = HTML + "<input type='checkbox' value='" + value + "'>" + value + " <br> \n"
+        value = prompt("Ingrese el texto de la opción " + i);
     }
+    cuestionarioSubsections.push(new cuestionarioSubsection("MC1", title, values));
+    console.log(cuestionarioSubsections);
 
     alert(HTML);
 }
 
 function Textfield() {
-    HTML = HTML + "<h3>" + prompt("Ingrese el título de la sección del textfield") + "</h3> \n"
-    let valor = "";
+    let title = prompt("Ingrese el título de la sección del textfield")
+    HTML = HTML + "<h3>" + title + "</h3> \n"
+    let value = "";
 
-    HTML = HTML + "<input type='textfield' value='" + valor + "'>" + valor + " <br> \n"
-    valor = prompt("Ingrese el texto");
+    cuestionarioSubsections.push(new cuestionarioSubsection("MC1", title));
+    console.log(cuestionarioSubsections);
+
+
+    HTML = HTML + "<input type='textfield' value='" + value + "'>" + value + " <br> \n"
+    value = prompt("Ingrese el texto");
 
     alert(HTML);
 }
